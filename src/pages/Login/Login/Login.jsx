f40719ab3055153c056aa186257adaa1d0f140c4
Jarin <khanjannath2020@gmail.com>
2023-05-03 21:15:52 +0600

    validate commit

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import './Login.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 const Login = () => {
+
+    const {signUser} = useContext(AuthContext);
+
+const handleLogin = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    signUser(email, password)
+    .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
     return (
         <div className='container mt-5'>
-       <Form className='mx-auto'>
+            <h2>Please Login</h2>
+       <Form onSubmit={handleLogin} className='mx-form'>
       <Form.Group className="mb-3 w-50" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" name='email' placeholder="Enter email" required/>
       </Form.Group>
 
       <Form.Group className="mb-3 w-50" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" name='password' placeholder="Password" required/>
       </Form.Group>
      
       <Button variant="dark" type="submit">
-        Submit
+        Login
       </Button>
  <div className='mt-2'>
  <Button  variant='light'>Google Sign-in</Button>
-<Button variant='light'>GitHub Sign-in</Button>
+<Button className='ms-2' variant='light'>GitHub Sign-in</Button>
+<br />
+<p className='mt-2'>Don't have an account yet? <Link to="/register">Register</Link></p>
+
  </div>
     </Form>      
         </div>
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,89 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+import { useState } from 'react';
 
 const Register = () => {
+
+    const [regError, setRegError] = useState('');
+
+    const {createUser} = useContext(AuthContext);
+
+    const handleRegister = event => {
+
+
+
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const photo = form.photo.value;
+
+        if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(password)) {
+            setRegError("password not valid need 6 character ");
+            return;
+          }
+
+          if(!email || email === ''){
+            setRegError('cannot be blank!')
+            return;
+          }
+
+        //   if( !password || password === ''){
+        //     setRegError('cannot be blank!')
+        //   }
+
+        console.log(name, email, password, photo)
+        createUser(email, password)
+        .then(result => {
+            const createdUser = result.user;
+            console.log(createdUser);
+            setRegError('')
+            setPassError('')
+            // event.target.reset();
+        })
+        .catch(error =>{
+            console.log(error.message);
+            // setRegError(error.message);
+            
+        })
+    }
+
     return (
-        <div>
-    <h2>This is register page</h2>        
-        </div>
+      <div className='container mt-5'>
+        <h2>Please Register</h2>
+        <Form onSubmit={handleRegister} className='mx-form'>
+        <Form.Group className="mb-3 w-50" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" name='name' placeholder="Enter name" required />
+         
+        </Form.Group>
+        <Form.Group className="mb-3 w-50" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" name='email' placeholder="Enter email"  />
+         
+        </Form.Group>
+  
+        <Form.Group className="mb-3 w-50" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" name='password' placeholder="Password"/>
+        </Form.Group>
+        <Form.Group className="mb-3 w-50" controlId="formBasicPhoto">
+          <Form.Label>Photo Url</Form.Label>
+          <Form.Control type="text" name='photo' placeholder="Photo Url" required/>
+        </Form.Group>
+       
+        <Button variant="dark" type="submit">
+          Register
+        </Button>
+        <br />
+<p className='mt-2'>Already have an account ? <Link to="/login">Login</Link></p>
+<p className='text-danger'>{regError}</p>
+
+      </Form>
+      </div>
     );
 };
 
